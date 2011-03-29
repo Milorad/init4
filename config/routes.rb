106207f957256f7dc18f0a7bd9f56470b44af630
Milorad Milutinovic <milorad.milutinovic.loki@gmail.com>
2011-03-29 14:26:48 +0200

    store controlled

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
+#---
+# Excerpted from "Agile Web Development with Rails, 4rd Ed.",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 Init4::Application.routes.draw do
+  get "store/index"
+
   resources :products
 
+  # ...
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -51,6 +62,9 @@ Init4::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  root :to => 'store#index', :as => 'store'
+
+  # ...
 
   # See how all your routes lay out with "rake routes"
 
