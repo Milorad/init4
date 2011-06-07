59082155acd2238db9d30d64bb1e044ec79b0293
Milorad Milutinovic <milorad.milutinovic.loki@gmail.com>
2011-06-08 00:03:19 +0200

    checkout

--- test/functional/line_items_controller_test.rb
+++ test/functional/line_items_controller_test.rb
@@ -1,11 +1,3 @@
-#---
-# Excerpted from "Agile Web Development with Rails, 4rd Ed.",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material, 
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose. 
-# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
-#---
 require 'test_helper'
 
 class LineItemsControllerTest < ActionController::TestCase
@@ -31,6 +23,17 @@ class LineItemsControllerTest < ActionController::TestCase
     assert_redirected_to store_path
   end
 
+  test "should create line_item via ajax" do
+  assert_difference('LineItem.count') do
+  xhr :post, :create, :product_id => products(:ruby).id
+  end
+
+  assert_response :success
+  assert_select_rjs :replace_html, 'cart' do
+  assert_select 'tr#current_item td', /Programming Ruby 1.9/
+  end
+  end
+
   test "should show line_item" do
     get :show, :id => @line_item.to_param
     assert_response :success
@@ -53,17 +56,5 @@ class LineItemsControllerTest < ActionController::TestCase
 
     assert_redirected_to line_items_path
   end
-
-  test "should create line_item via ajax" do
-    assert_difference('LineItem.count') do
-      xhr :post, :create, :product_id => products(:ruby).id
-    end 
-
-    assert_response :success
-    assert_select_rjs :replace_html, 'cart' do
-      assert_select 'tr#current_item td', /Programming Ruby 1.9/
-    end
-  end
-
-
+  assert_redirected_to cart_path(assigns(:line_item).cart)
 end
