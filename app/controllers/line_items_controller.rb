b85b99de67ccfb21824a419fe3a0fda5be775f63
Milorad Milutinovic <milorad.milutinovic.loki@gmail.com>
2011-06-07 22:15:32 +0200

    chap11b

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,11 +1,3 @@
-#---
-# Excerpted from "Agile Web Development with Rails, 4rd Ed.",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material, 
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose. 
-# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
-#---
 class LineItemsController < ApplicationController
   # GET /line_items
   # GET /line_items.xml
@@ -55,6 +47,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to(store_url) }
+        format.js
         format.xml  { render :xml => @line_item,
           :status => :created, :location => @line_item }
       else
